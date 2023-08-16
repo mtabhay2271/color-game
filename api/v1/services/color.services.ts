4a29a5a4176cca8349271653e023c939ec1e54f8
Abhay <abhay.upadhyay@karyashala.in>
2023-08-17 00:45:57 +0530

    old game code remove

--- api/v1/services/color.services.ts
+++ api/v1/services/color.services.ts
@@ -12,26 +12,8 @@ class dataServicesData {
 
   add = async (req: Request): Promise<ICommonServices> => {
     try {
-      // console.log("addddddddddddddddddd")
 
       let getGames = await Color.find({});
-      // let FoundJoin = await Join.find({ num: (getGames.length + 1) })
-      // // console.log(FoundJoin.length,"FoundJoin.length",FoundJoin);
-      // let resultData = { green: 0, red: 0, yellow: 0 }
-      // if (FoundJoin) {
-      //   // console.log("11111111")
-      //   FoundJoin.forEach((e: JoinModel) => {
-      //     if (e.color == 1) {
-      //       resultData.green = resultData.green + e.amount;
-      //     } else if (e.color == 2) {
-      //       resultData.red = resultData.red + e.amount;
-      //     } else {
-      //       resultData.yellow = resultData.yellow + e.amount;
-      //     }
-      //   })
-      // }
-      // console.log(resultData);
-
       const resultData1 = await Join.aggregate([
         { $match: { num: getGames.length + 1 } },
         {
@@ -47,11 +29,11 @@ class dataServicesData {
 
       resultData1.forEach((entry) => {
         if (entry._id === 1) {
-          aggregatedResult.green = entry.totalAmount*2;
+          aggregatedResult.green = entry.totalAmount * 2;
         } else if (entry._id === 2) {
-          aggregatedResult.red = entry.totalAmount*3;
+          aggregatedResult.red = entry.totalAmount * 3;
         } else {
-          aggregatedResult.yellow = entry.totalAmount*5;
+          aggregatedResult.yellow = entry.totalAmount * 5;
         }
       });
 
@@ -59,12 +41,8 @@ class dataServicesData {
 
       let sortedResult = Object.entries(aggregatedResult).sort(([, v1], [, v2]) => v1 - v2);
       let result = 1;
-      // console.log("sortedResult", sortedResult);
       let newArray = sortedResult.filter(e => { return e[1] == sortedResult[0][1] })
-      // console.log("newArray", newArray);
-      // sortedResult.pop();
       let result1 = newArray[Math.floor(Math.random() * newArray.length)];
-      //  console.log("result1",result1,"sortedResult",sortedResult)
 
       if (result1[0] == 'green') {
         result = 1;
