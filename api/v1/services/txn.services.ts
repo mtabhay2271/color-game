0845d249ae1688cfe8cbf824db91b55c4da6ad1f
abhay.upadhyay@adglobal360.com-at-288519183534 <abhay.upadhyay@adglobal360.com>
2023-09-16 12:38:02 +0530

    10 on add amount

--- api/v1/services/txn.services.ts
+++ api/v1/services/txn.services.ts
@@ -144,9 +144,10 @@ class dataServicesData {
       let data: any = await TxnModel.findByIdAndUpdate(id, { $set: { status: 1 } }, { new: true });
       if (data) {
         if (!data?.widhrawal) {
+          let actualAmount = data.amount < 1000 ? data.amount : data.amount + ((data.amount * 10) / 100)
           let user = await Users.findByIdAndUpdate(data.userId, {
             $inc: {
-              availableAmount: data.amount
+              availableAmount: actualAmount
             }
           }, { new: true })
           if (!user?.paymentStatus) {
