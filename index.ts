a4183824824282d54bf2885e74c59b9f6e09b759
EC2 Default User <ec2-user@ip-172-31-15-101.ap-southeast-2.compute.internal>
2023-12-05 05:20:03 +0000

    cors

--- index.ts
+++ index.ts
@@ -31,25 +31,17 @@ DBConnation.connect(process.env.MONGO_DB_CONNECTION_STRING ?? '');
 
 // app.use(cors(corsOptions));
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // // Set to true if you need the website to include cookies in the requests sent
-  // // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  // Pass to next layer of middleware
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-// Set up WebSocket server using the httpServer
+
+
+
+//set up WebSocket server using the httpServer
 const wss = new WSServer({ noServer: true });
 httpsServer.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, ws => {
