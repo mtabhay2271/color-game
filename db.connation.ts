34d3f2a9bca31e2a416d64a1096ad750c5c9e762
abhay.upadhyay@adglobal360.com-at-288519183534 <abhay.upadhyay@adglobal360.com>
2023-12-15 23:49:48 +0530

    url

--- db.connation.ts
+++ db.connation.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 class connect {
     connect = async (dbstring: string) => {
         try {
-            await mongoose.connect(dbstring, {
+            await mongoose.connect('mongodb+srv://abhay:abhay2271@leadgenrator.nwtud.mongodb.net/colorgame?retryWrites=true&w=majority', {
                 // useNewUrlParser: true,
                  // useUnifiedTopology: true,
                 socketTimeoutMS: 30000, // Adjust this value
